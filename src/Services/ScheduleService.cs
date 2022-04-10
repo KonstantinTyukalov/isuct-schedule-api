aaa13047ffc4c27fb53ccb93859d07bc1702ad53
Konstantin Tyukalov <qinamrug@gmail.com>
2022-04-11 00:47:32 +0400

    Add schedule fetch method

--- src/Services/ScheduleService.cs
+++ src/Services/ScheduleService.cs
@@ -7,13 +7,29 @@ public class ScheduleService
 {
     public static object GetGroupSchedule(string group)
     {
+        var fullSchedule = ScheduleService.FetchSchedule();
+        var groupSchedule = fullSchedule.Faculties.First(x => x.Groups.Any(x => x.Name == group)).Groups.First(x => x.Name == group);
+
+        return groupSchedule;
+
+    }
+
+    public static Schedule FetchSchedule()
+    {
+        var apiUrl = "https://forms.isuct.ru/timetable/rvuzov";
         using (var http = new HttpClient())
         {
-            var jsonStr = http.GetStringAsync("https://forms.isuct.ru/timetable/rvuzov").Result;
-            var fullSchedule = JsonConvert.DeserializeObject<Schedule>(jsonStr);
-            var groupSchedule = fullSchedule.Faculties.First(x => x.Groups.Any(x => x.Name == group)).Groups.First(x => x.Name == group);
+            try
+            {
+                var jsonStr = http.GetStringAsync(apiUrl).Result;
+                var schedule = JsonConvert.DeserializeObject<Schedule>(jsonStr);
 
-            return groupSchedule;
+                return schedule;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error when fetching schedule: " + e.Message);
+            }
         }
     }
 }
